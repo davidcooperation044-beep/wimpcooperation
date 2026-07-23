7924b1f393691d137216acb195d9dd9dddd5fa97
davidcooperation044-beep <davidcooperation044@gmail.com>
2026-07-23 20:06:52 +0100

    Create email.js

---
+++ utils/email.js
@@ -0,0 +1,43 @@
+const { Resend } = require('resend');
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+async function sendWelcomeEmail(email, password, role) {
+
+    await resend.emails.send({
+
+        from: process.env.FROM_EMAIL,
+
+        to: email,
+
+        subject: 'Welcome to Wimpy Portal',
+
+        html: `
+            <h2>Congratulations!</h2>
+
+            <p>Your application has been accepted.</p>
+
+            <p><strong>Role:</strong> ${role}</p>
+
+            <p><strong>Email:</strong> ${email}</p>
+
+            <p><strong>Password:</strong> ${password}</p>
+
+            <p>
+                Login here:
+                <br>
+                <a href="http://localhost:3000/login.html">
+                    Wimpy Portal
+                </a>
+            </p>
+
+            <p>Please change your password after your first login.</p>
+        `
+
+    });
+
+}
+
+module.exports = {
+    sendWelcomeEmail
+};
